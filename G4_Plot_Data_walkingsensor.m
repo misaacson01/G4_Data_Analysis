89ede10aca738f88f4928fa4c5e554122f4d672f
Matt Isaacson <misaacson01@gmail.com>
2019-07-26 10:56:33 -0400

    Fixed default plotting structure, reduced whitespace, added option to overlap 2 conditions on the same axis

--- G4_Plot_Data_walkingsensor.m
+++ G4_Plot_Data_walkingsensor.m
@@ -1,5 +1,5 @@
-function G4_Plot_Data_walkingsensor(exp_folder, trial_options, CL_conds, OL_conds, TC_conds)
-%FUNCTION G4_Plot_Data_walkingsensor(exp_folder, trial_options, CL_conds, OL_conds, TC_conds)
+function G4_Plot_Data_walkingsensor(exp_folder, trial_options, CL_conds, OL_conds, TC_conds, overlap)
+%FUNCTION G4_Plot_Data_walkingsensor(exp_folder, trial_options, CL_conds, OL_conds, TC_conds, overlap)
 % 
 % Inputs:
 % exp_folder: path containing G4_Processed_Data.mat file
@@ -7,6 +7,7 @@ function G4_Plot_Data_walkingsensor(exp_folder, trial_options, CL_conds, OL_cond
 % CL_conds: matrix of closed-loop (CL) conditions to plot as histograms
 % OL_conds: matrix of open-loop (OL) conditions to plot as timeseries
 % TC_conds: matrix of open-loop conditions to plot as tuning curves (TC)
+% overlap: logical (0 default); plots every 2 rows of conditions on a single row of axes in different colors
 
 
 %% user-defined parameters
@@ -16,8 +17,10 @@ OL_datatypes = {'Turning','Forward'}; %datatypes to plot as timeseries
 TC_datatypes = {'Turning','Forward'}; %datatypes to plot as tuning curves
 
 %specify plot properties
-rep_Color = [0.5 0.5 0.5];
-mean_Color = [0 0 0];
+rep_Color = [0.5 0.5 1];
+mean_Color = [0 0 1];
+rep_Color2 = [1 0.5 0.5];
+mean_Color2 = [1 0 0];
 rep_LineWidth = 0.05;
 mean_LineWidth = 1;
 subtitle_FontSize = 8;
@@ -39,12 +42,33 @@ load(fullfile(exp_folder,Data_name));
 
 %create default matrices for plotting all conditions
 if nargin<5 
-    CL_conds = find(Data.conditionModes==4); %all closed-loop modes
-    OL_conds = find(Data.conditionModes~=4); %all open-loop modes
+    default_W = [2 2 2 2 3 3 3 3 3 3 3 3 4 4 4 4 4 4 4 4 5 5 5 5 5 5 5 5 5 5]; %width of figure by number of subplots
+    default_H = [1 1 2 2 2 2 3 3 3 4 4 4 4 4 4 4 5 5 5 5 5 5 5 5 5 6 6 6 6 6]; %height of figure by number of subplots
+    
+    %find all open-loop conditions, organize into block
+    conds_vec = find(Data.conditionModes~=4); 
+    num_conds = length(OL_conds_vec);
+    W = default_W(min([num_conds length(default_W)])); %get number of subplot columns (up to default limit)
+    H = default_H(min([num_conds length(default_W)])); %get number of subplot rows
+    D = ceil(num_conds/length(default_W)); %number of figures
+    OL_conds = nan([W H D]);
+    OL_conds(1:num_conds) = conds_vec;
+    OL_conds = permute(OL_conds,[2 1 3]);
+    
+    %find all closed-loop conditions, organize into block
+    conds_vec = find(Data.conditionModes=4); 
+    num_conds = length(CL_conds_vec);
+    W = default_W(min([num_conds length(default_W)])); %get number of subplot columns (up to default limit)
+    H = default_H(min([num_conds length(default_W)])); %get number of subplot rows
+    D = ceil(num_conds/length(default_W)); %number of figures
+    CL_conds = nan([W H D]);
+    CL_conds(1:num_conds) = conds_vec;
+    CL_conds = permute(CL_conds,[2 1 3]);
+    
     TC_conds = []; %by default, don't plot any tuning curves
-    CL_conds = reshape(CL_conds,[round(sqrt(numel(CL_conds))) ceil(sqrt(numel(CL_conds)))])';
-    OL_conds = reshape(OL_conds,[round(sqrt(numel(OL_conds))) ceil(sqrt(numel(OL_conds)))])';
+    overlap = 0; %by default, don't overlap multiple conditions on same plot
 end
+overlap = logical(overlap);
 
 %get datatype indices
 num_OL_datatypes = length(OL_datatypes);
@@ -96,14 +120,14 @@ if ~isempty(CL_conds)
     num_figs = size(CL_conds,3);
     for d = CL_inds
         for fig = 1:num_figs
-            num_plot_rows = max(nansum(CL_conds(:,:,fig)>0));
+            num_plot_rows = (1-overlap/2)*max(nansum(CL_conds(:,:,fig)>0));
             num_plot_cols = max(nansum(CL_conds(:,:,fig)>0,2));
-            figure()
+            figure('Position',[100 100 540 540*(num_plot_rows/num_plot_cols)])
             for row = 1:num_plot_rows
                 for col = 1:num_plot_cols
-                    cond = CL_conds(row,col,fig);
+                    cond = CL_conds(row*(1+overlap)-1,col,fig);
                     if cond>0
-                        subplot(num_plot_rows, num_plot_cols, col+num_plot_cols*(row-1))
+                        better_subplot(num_plot_rows, num_plot_cols, col+num_plot_cols*(row-1))
                         [~, ~, num_reps, num_positions] = size(Data.histograms);
                         x = circshift(1:num_positions,[1 num_positions/2]);
                         x(x>x(end)) = x(x>x(end))-num_positions;
@@ -113,6 +137,14 @@ if ~isempty(CL_conds)
                         plot(x,nanmean(tmpdata),'Color',mean_Color,'LineWidth',mean_LineWidth)
                         ylim(ylimits(d,:));
                         title(['Condition #' num2str(cond)],'FontSize',subtitle_FontSize)
+                    end
+                    if overlap==1
+                        cond = CL_conds(row*(1+overlap),col,fig);
+                        if cond>0
+                            tmpdata = circshift(squeeze(Data.histograms(d,cond,:,:)),[1 num_positions/2]);
+                            plot(repmat(x',[1 num_reps]),tmpdata','Color',rep_Color2,'LineWidth',rep_LineWidth);
+                            plot(x,nanmean(tmpdata),'Color',mean_Color2,'LineWidth',mean_LineWidth)
+                        end
                     end
                 end
             end
@@ -127,21 +159,27 @@ if ~isempty(OL_conds)
     %loop for different data types
     for d = OL_inds
         for fig = 1:num_figs
-            num_plot_rows = max(nansum(OL_conds(:,:,fig)>0));
+            num_plot_rows = (1-overlap/2)*max(nansum(OL_conds(:,:,fig)>0));
             num_plot_cols = max(nansum(OL_conds(:,:,fig)>0,2));
-            figure()
+            figure('Position',[100 100 540 540*(num_plot_rows/num_plot_cols)])
             for row = 1:num_plot_rows
                 for col = 1:num_plot_cols
-                    cond = OL_conds(row,col,fig);
+                    cond = OL_conds(row*(1+overlap)-1,col,fig);
                     if ~isnan(cond)
-                        subplot(num_plot_rows, num_plot_cols, col+num_plot_cols*(row-1))
-                        subplot(num_plot_rows, num_plot_cols, col+num_plot_cols*(row-1))
+                        better_subplot(num_plot_rows, num_plot_cols, col+num_plot_cols*(row-1))
                         plot(repmat(Data.timestamps',[1 num_reps]),squeeze(Data.timeseries(d,cond,:,:))','Color',rep_Color,'LineWidth',rep_LineWidth);
                         hold on
                         plot(Data.timestamps,squeeze(nanmean(Data.timeseries(d,cond,:,:),3)),'Color',mean_Color,'LineWidth',mean_LineWidth);
                         ylim(ylimits(d,:));
                         xlim([min(Data.timestamps) max(Data.timestamps)])
                         title(['Condition #' num2str(cond)],'FontSize',subtitle_FontSize)
+                    end
+                    if overlap==1
+                        cond = CL_conds(row*(1+overlap),col,fig);
+                        if cond>0
+                            plot(repmat(Data.timestamps',[1 num_reps]),squeeze(Data.timeseries(d,cond,:,:))','Color',rep_Color2,'LineWidth',rep_LineWidth);
+                            plot(Data.timestamps,squeeze(nanmean(Data.timeseries(d,cond,:,:),3)),'Color',mean_Color2,'LineWidth',mean_LineWidth);
+                        end
                     end
                 end
             end
@@ -155,17 +193,23 @@ if ~isempty(TC_conds)
     %loop for different data types
     for d = TC_inds
         for fig = 1:num_figs
-            num_plot_rows = max(nansum(TC_conds(:,:,fig)>0));
-            figure()
+            num_plot_rows = (1-overlap/2)*max(nansum(TC_conds(:,:,fig)>0));
+            figure('Position',[100 100 540/num_plot_rows 540])
             for row = 1:num_plot_rows
-                conds = TC_conds(row,:,fig);
+                conds = TC_conds(row*(1+overlap)-1,:,fig);
                 conds(isnan(conds)) = [];
-                subplot(num_plot_rows, 1, row)
+                better_subplot(num_plot_rows, 1, row)
                 plot(squeeze(Data.summaries(d,conds,:)),'Color',rep_Color,'LineWidth',rep_LineWidth);
                 hold on
                 plot(nanmean(Data.summaries(d,conds,:),3),'Color',mean_Color,'LineWidth',mean_LineWidth);
                 ylim(ylimits(d,:));
                 title(['Condition #' num2str(cond)],'FontSize',subtitle_FontSize)
+                if overlap==1
+                    conds = TC_conds(row*(1+overlap),:,fig);
+                    conds(isnan(conds)&&conds==0) = [];
+                    plot(squeeze(Data.summaries(d,conds,:)),'Color',rep_Color2,'LineWidth',rep_LineWidth);
+                    plot(nanmean(Data.summaries(d,conds,:),3),'Color',mean_Color2,'LineWidth',mean_LineWidth);
+                end
             end
         end
     end
